6bcc6fae5c82ae8953acac44ca440ef53438de0c
Kaylin-C <kayc260@gmail.com>
2017-07-25 09:56:42 -0400

    3 motors in fading

--- FadingMotor/FadingMotor.ino
+++ FadingMotor/FadingMotor.ino
@@ -18,7 +18,9 @@
  */
 
 
-int motorpin = 9;    // LED connected to digital pin 9
+int motorpin1 = 9;    // LED connected to digital pin 9
+int motorpin2 = 10;    // LED connected to digital pin 9
+int motorpin3 = 11;    // LED connected to digital pin 9
 
 void setup() {
   // nothing happens in setup
@@ -28,7 +30,9 @@ void loop() {
   // fade in from min to max in increments of 5 points:
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
     // sets the value (range from 0 to 255):
-    analogWrite(motorpin, fadeValue);
+    analogWrite(motorpin1, fadeValue);
+    analogWrite(motorpin2, fadeValue);
+    analogWrite(motorpin3, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
     delay(30);
   }
@@ -36,7 +40,9 @@ void loop() {
   // fade out from max to min in increments of 5 points:
   for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
     // sets the value (range from 0 to 255):
-    analogWrite(motorpin, fadeValue);
+    analogWrite(motorpin1, fadeValue);
+    analogWrite(motorpin2, fadeValue);
+    analogWrite(motorpin3, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
     delay(30);
   }
